e0da60d6b89835dc3f4274a102642ddf63e2731d
Ray Villalobos <ray@planetoftheweb.com>
2021-09-08 03:55:34 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let total = 0;
+let count = 0;
+const $ = document
+  .querySelector.bind(document);
+
+$('#moneyBtn')
+  .addEventListener('click', () => {
+    count++;
+    if (count % 3 == 0) {
+      total += 10;
+    }
+    $('#count').innerText = count;
+    $('#total').innerText = total;
+  });
+
