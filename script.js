d2a737fa3fbabe9d15f02f7e5a3799eccb80d5aa
Ray Villalobos <ray@planetoftheweb.com>
2021-09-08 04:00:43 -0400

    beginning state

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 let total = 0;
 let count = 0;
-const $ = document
-  .querySelector.bind(document);
-const $$ = document
-  .querySelectorAll.bind(document);
 
-$('#moneyBtn')
+document.querySelector('#moneyBtn')
   .addEventListener('click', () => {
     count++;
     if (count % 3 == 0) {
       total += 10;
     }
-    $('#count').innerText = count;
-    $('#total').innerText = total;
+    document.querySelector('#count').innerText = count;
+    document.querySelector('#total').innerText = total;
   });
 
